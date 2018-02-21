4be00dfda89920d48a506e182f5413b62ed2141e
Dmitrij Kestner <dima.kestner@yandex.ru>
2018-02-21 18:51:57 +0300

    'current'

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var open = [];
+  var close = [];
+  var i,j = 0;
+  var check = 0;
+  var tmp = [];
+  for(i=0; i<bracketsConfig.length;i++){
+    open[i] = bracketsConfig[i][0];
+    close[i] = bracketsConfig[i][1];
+  }
+ // console.log(open);
+ // console.log(close);
+  for (i=0; i<str.length;i++){
+    for(j=0;j<open.length; j++){
+      if(str[i]==open[j]){
+      //  console.log(str[i]);
+        tmp.push(j);
+        //console.log(tmp);
+        check ++;
+      }
+      if(str[i]==close[j]){
+       // console.log(str[i]);
+        if(j==tmp.pop()){
+          check--;
+        }
+        else return false;
+      }
+    }
+   // console.log('current check=' , check);
+    if(check<0) return false;
+  }
+  //console.log('check =' , check);
+  if(check == 0) return true;
+  else return false;
 }
