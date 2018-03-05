f11d4ce2ef7f87dd9419837aeb040f3b82c28c16
Dmitrij Kestner <dima.kestner@yandex.ru>
2018-03-05 18:20:56 +0300

    'finally'

--- src/index.js
+++ src/index.js
@@ -4,32 +4,37 @@ module.exports = function check(str, bracketsConfig) {
   var i,j = 0;
   var check = 0;
   var tmp = [];
+  var k=0;
   for(i=0; i<bracketsConfig.length;i++){
     open[i] = bracketsConfig[i][0];
     close[i] = bracketsConfig[i][1];
   }
- // console.log(open);
- // console.log(close);
+
   for (i=0; i<str.length;i++){
     for(j=0;j<open.length; j++){
-      if(str[i]==open[j]){
-      //  console.log(str[i]);
+      if(open[j]==close[j]&&str[i]==close[j]){
+        if(str[i]==close[tmp[tmp.length-1]]){
+          tmp.pop();
+          check--;
+        }
+        else{
+          tmp.push(j);
+          check++;
+        }
+      }
+      else if(str[i]==open[j]){
         tmp.push(j);
-        //console.log(tmp);
         check ++;
       }
-      if(str[i]==close[j]){
-       // console.log(str[i]);
+      else if(str[i]==close[j]){
         if(j==tmp.pop()){
           check--;
         }
         else return false;
       }
     }
-   // console.log('current check=' , check);
     if(check<0) return false;
   }
-  //console.log('check =' , check);
   if(check == 0) return true;
   else return false;
 }
